e050ead054176a4f77acedde358179abaf57d1f8
Aleksandar <aleksandar12077171@gmail.com>
2021-01-25 18:22:35 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -280,3 +280,13 @@ var siteDatePicker = function() {
 };
 siteDatePicker();
 
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
